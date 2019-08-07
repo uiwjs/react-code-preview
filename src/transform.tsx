6315d4ebc7dc760d77bac0abcec8ed92cd73ccc7
jaywcjlove <398188662@qq.com>
2019-08-07 12:06:51 +0800

    fix: Remove all import declaration.

--- src/transform.tsx
+++ src/transform.tsx
@@ -9,16 +9,13 @@ export async function BabelTransform(input: string) {
         return {
           name: 'transform-remove-all-import',
           visitor: {
-            // https://babeljs.io/docs/en/babel-types#importspecifier
-            ImportSpecifier(local: any) {
-              if (local.parent && local.parent.source && local.parent.source.extra.rawValue) {
-                if (local.parent.specifiers) {
-                  local.parent.specifiers.forEach((item: any) => {
-                    specifiers.push(item.imported.name);
-                  });
-                }
-                local.parentPath.remove();
-              }
+            /**
+             * https://babeljs.io/docs/en/babel-types#importdeclaration
+             * @param specifiers
+             * @param source 
+             */
+            ImportDeclaration(specifiers: any, source: string) {
+              specifiers.remove();
             },
           },
         };
