4db9ad34eff6aeee4c01a5ca649902919b55e9b9
jaywcjlove <398188662@qq.com>
2020-04-16 01:08:15 +0800

    chore(deps): Replace uiw to @uiw/react-split.

--- .kktrc.ts
+++ .kktrc.ts
@@ -1,4 +1,8 @@
 import path from 'path';
+import { OptionConf } from 'kkt';
+import webpack from 'webpack';
+
+type Webpack = typeof webpack;
 
 export const moduleScopePluginOpts = [
   path.resolve(process.cwd(), 'README.md'),
@@ -9,7 +13,7 @@ export const loaderOneOf = [
   require.resolve('@kkt/loader-less')
 ];
 
-export default (conf, opts, webpack) => {
+export default (conf: webpack.Configuration, opts: OptionConf, webpack: Webpack) => {
   const pkg = require(path.resolve(process.cwd(), 'package.json'));
   conf.module.rules.map((item) => {
     if (item.oneOf) {
