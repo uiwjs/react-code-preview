4ce1d537138c193b8fbfc800cd8953476f3def78
jaywcjlove <398188662@qq.com>
2021-08-30 16:03:23 +0800

    test: update test case.

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,19 +1,23 @@
 /**
  * @jest-environment jsdom
  */
+/* eslint-disable jest/no-conditional-expect */
+// @ts-ignore
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import CodePreview from '../';
 
 describe('<CodePreview />', () => {
-  it('Should output a Button', () => {
-    const component = TestRenderer.create(
-      <CodePreview />
-    );
+  it('Should output a CodePreview', () => {
+    const component = TestRenderer.create(<CodePreview />);
     let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-    if (tree) {
-      // expect(tree.type).toBe('button');
+    if (tree && !Array.isArray(tree)) {
+      expect(tree.type).toEqual('div');
+      expect(tree.props.className).toEqual('w-split w-code-preview w-code-preview-bordered w-split-horizontal');
+      expect(tree.props.style).toEqual({
+        flex: 1
+      });
+      expect(tree.children!.length).toEqual(3);
     }
   });
 });
