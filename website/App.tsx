16dbcdb707fea7598e52b527b064dff365e7a168
jaywcjlove <398188662@qq.com>
2022-01-18 13:06:46 +0800

    website: document display version information.

--- website/App.tsx
+++ website/App.tsx
@@ -5,17 +5,19 @@ import DocumentStr from '../README.md';
 import styles from './App.module.less';
 import Example from './Example';
 
-export default class App extends React.PureComponent<{}, {}> {
-  render() {
-    let DocumentStrSource = DocumentStr;
-    if (DocumentStrSource) DocumentStrSource = DocumentStr.replace(/([\s\S]*)<!--dividing-->/, '');
-    return (
-      <div className={styles.warpper}>
-        <GitHubCorners fixed href="https://github.com/uiwjs/react-code-preview" target="__blank" />
-        <h1 className={styles.title}>React Code Preview</h1>
-        <Example />
-        <Markdown style={{ paddingTop: 25 }} source={DocumentStrSource} />
-      </div>
-    );
-  }
+export default function App() {
+  // @ts-ignore
+  const version = VERSION;
+  let DocumentStrSource = DocumentStr;
+  if (DocumentStrSource) DocumentStrSource = DocumentStr.replace(/([\s\S]*)<!--dividing-->/, '');
+  return (
+    <div className={styles.warpper}>
+      <GitHubCorners fixed href="https://github.com/uiwjs/react-code-preview" target="__blank" />
+      <h1 className={styles.title}>
+        React Code Preview <sup>{version}</sup>
+      </h1>
+      <Example />
+      <Markdown style={{ paddingTop: 25 }} source={DocumentStrSource} />
+    </div>
+  );
 }
