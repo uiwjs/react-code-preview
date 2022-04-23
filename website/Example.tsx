eeafb8e8e6e7afb8eda8996dc7db6923463ca2b3
SunLxy <1011771396@qq.com>
2022-04-23 13:02:15 +0800

    fix:修改案例

--- website/Example.tsx
+++ website/Example.tsx
@@ -7,7 +7,7 @@ const defaultCode = `
 import { Button, Divider, Icon } from 'uiw';
 import ReactDOMClient from 'react-dom/client';
 
-ReactDOM.render(
+ReactDOMClient.createRoot(_mount_).render(
   <div>
     <Button type="primary">主要按钮</Button>
     <Button type="success">成功按钮</Button>
@@ -16,7 +16,6 @@ ReactDOM.render(
     <Button type="light">亮按钮</Button>
     <Button type="dark">暗按钮</Button>
   </div>,
-  _mount_
 );`;
 
 const Example = () => {
