e6f094d7a2df71994138a8b7b292bbad42a4defd
Darwish <38754760+LonelyFellas@users.noreply.github.com>
2024-03-25 15:43:21 +0800

    feat: Add an additional 'sourceState' props attribute to the component (#236)
    * fix: fixed up ts error on `/website`
    
    * feat(deps): 增加@types react|react-dom to ^18.2.0 version
    
    * feat: Add an additional 'sourceState' props attribute to the component.
    
    * fix: fixed up eslint error
    
    * fix: Option 'emitDeclarationOnly' cannot be specified with option 'noEmit'.

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -7,6 +7,7 @@ describe('<CodePreview />', () => {
     const component = TestRenderer.create(<CodePreview />);
     let tree = component.toJSON();
     if (tree && !Array.isArray(tree)) {
+      /* eslint-disable jest/no-conditional-expect */
       expect(tree.type).toEqual('div');
       expect(tree.props.className).toEqual('w-split w-code-preview w-code-preview-bordered w-split-horizontal');
       expect(tree.props.style).toEqual({
